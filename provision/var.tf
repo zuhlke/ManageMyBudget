d0b44a4c23bdb5581d5ee483cdbe106abfeaac66
ajum <chemistric-05@mail.ru>
2019-10-30 13:41:43 +0000

    (feat): Add script to configure S3 bucket for static website hosting

---
+++ provision/var.tf
@@ -0,0 +1,9 @@
+variable "hosting-bucket-name" {
+  default = "manage-my-budget"
+  description = "A bucket name for hosting the static website"
+}
+
+variable "logging-bucket-name" {
+  default = "manage-my-budget-logs"
+  description = "A bucket name for storing logs"
+}
