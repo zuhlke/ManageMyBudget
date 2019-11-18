80bc29246a9770a674fbee42c96741ba7841ff9d
ajum <chemistric-05@mail.ru>
2019-11-18 12:03:17 +0000

    #38 Remove TODO comments

--- packages/front-end/terraform/lambda.tf
+++ packages/front-end/terraform/lambda.tf
@@ -21,14 +21,6 @@ resource "aws_lambda_function" "lambda" {
   runtime = "nodejs8.10"
 
   vpc_config {
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibilty in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     subnet_ids         = flatten([module.vpc.private_subnets])
     security_group_ids = [aws_security_group.sg.id]
   }
