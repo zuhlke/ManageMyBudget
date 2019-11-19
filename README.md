1d16f6be5ed4f9bd3237051e9f62a7e289e04750
Anna Slastnikova <anna.slastnikova@gmail.com>
2019-11-19 11:24:42 +0000

    add instruction on how to create tfstate bucket before applying terraform; move terraform for tfstate out of main TF code

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 
 Managing budget for vulnerable people.
 
+## Infrastructure deployment
+
+Before running `terraform apply` please create tfstate bucket using the AWS CLI:
+
+```
+aws s3api create-bucket --bucket <tfstate bucket Name> --region eu-west-2
+```
+
+It'll create the required S3 bucket with versioining enabled.
+
+In future this will be automated as part of CI/CD pipeline.
+
 ### Installation
 To install, run:
 ```
---
+++ packages/front-end/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${var.project-number}-${var.terraform-state-bucket}"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "${var.project-number}-${var.terraform-lock-dynamodb}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "z10016-manage-my-budget-tf-state"
+    key ="global/s3/terraform.tfstate"
+    region = "eu-west-2"
+
+    dynamodb_table = "z10016-manage-my-budget-tf-lock"
+    encrypt = true
+  }
+}
